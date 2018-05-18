040b48cb843144fa66ee07bf554165672c929a71
ujjwalagr <ujjwalagrawal53@gmail.com>
2018-05-18 15:58:21 +0530

    Initial commit

---
+++ app/src/main/java/com/unionsapp/contentselectionscreen/MainActivity.java
@@ -0,0 +1,13 @@
+package com.unionsapp.contentselectionscreen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
